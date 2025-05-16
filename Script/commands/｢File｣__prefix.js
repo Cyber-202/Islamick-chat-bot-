a6ad40197527d384d1dc3c153e3a632ecf847b18
Cyber-202 <ohid4167@gmail.com>
2025-05-16 23:03:55 +0600

    Update ｢File｣__prefix.js

--- Script/commands/｢File｣__prefix.js
+++ Script/commands/｢File｣__prefix.js
@@ -25,10 +25,10 @@ module.exports.handleEvent = async ({ event, api, Threads }) => {
     if (body === i.toUpperCase() | body === i | str === body) {
 const prefix = threadSetting.PREFIX || global.config.PREFIX;
       if (data.PREFIX == null) {
-        return out(`This Is My Prefix ⇉ [ ${prefix} ]\n\n𝐎𝐖𝐍𝐄𝐑:- ☞ULLASH × ☆𝐀𝐁𝐇𝐑𝐀𝐍𝐈𝐋☆\n\n𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 ULLASH ッ\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝:- ☞ www.facebook.com/100000654976375\n
-For Any Kind Of Help Contact On massager×✓ m.me/100000654976375 😇`)
+        return out(`This Is My Prefix ⇉ [ ${prefix} ]\n\n𝐎𝐖𝐍𝐄𝐑:- ☞OHID × ☆CHOUDURY☆\n\n𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 OHID ッ\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝:- ☞ www.facebook.com/davil.ohid.202\n
+For Any Kind Of Help Contact On massager×✓ m.me/davil.ohid.202 😇`)
       }
-      else return out('️️️️️️️️️️️️️️️️️️️️️️️️️️️This Is My Prefix ⇉ [ ${prefix} ]  \n\n𝐎𝐖𝐍𝐄𝐑:- ☞ULLASH × ☆𝐀𝐁𝐇𝐑𝐀𝐍𝐈𝐋☆\n\n𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 ULLASH ッ\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝:- ☞ www.facebook.com/100086680386976\nFor Any Kind Of Help Contact On massager×✓ m.me/100000654976375 ' + data.PREFIX)
+      else return out('️️️️️️️️️️️️️️️️️️️️️️️️️️️This Is My Prefix ⇉ [ ${prefix} ]  \n\n𝐎𝐖𝐍𝐄𝐑:- ☞OHID × ☆CHOUDURY☆\n\n𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 OHID ッ\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝:- ☞ www.facebook.com/davil.ohid.202\nFor Any Kind Of Help Contact On massager×✓ m.me/davil.ohid.202 ' + data.PREFIX)
     }
 
   });
